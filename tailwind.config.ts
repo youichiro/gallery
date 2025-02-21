c38ba5529b44a9f32c5ae8824ec0bba2f53551ec
youichiro <cinnamon416@gmail.com>
2025-02-21 18:21:54 +0900

    show top page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,13 +6,6 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 } satisfies Config;
