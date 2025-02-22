3eabbaff6f9e56f2ecf552af996ed044ca687018
youichiro <cinnamon416@gmail.com>
2025-02-22 15:08:00 +0900

    add vercel speed insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
 
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
