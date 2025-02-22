94fc9a18819b515c5d5961f356bef252299f72f8
youichiro <cinnamon416@gmail.com>
2025-02-22 14:14:21 +0900

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ja" className="scroll-smooth">
       <body>
         {children}
       </body>
