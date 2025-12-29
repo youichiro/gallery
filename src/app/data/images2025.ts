32011814c3439d167628ee9d466f831cddddbf78
youichiro ogawa <cinnamon416@gmail.com>
2025-12-29 19:36:23 +0900

    リファクタリング (#3)
    * add comments
    
    * add Top component
    
    * add ExpandedImage component
    
    * add Gallery component
    
    * 縦向きの画像に対応する
    
    * rename image directory
    
    * 縦横判定を追加
    
    * 定数を切り出す
    
    * 画像の縦横判定を追加
    
    * タブを追加
    
    * add Tabs component
    
    * fix
    
    * add 2025 images
    
    * buildが失敗するのを解消

---
+++ src/app/data/images2025.ts
@@ -0,0 +1,13 @@
+export const images2025 = [
+  "/images/2025/landscape/DSC_2844.JPG",
+  "/images/2025/landscape/DSC07886.jpeg",
+  "/images/2025/vertical/IMG_1063.JPG",
+  "/images/2025/landscape/DSC09781-2.jpg",
+  "/images/2025/vertical/IMG_1064.JPG",
+  "/images/2025/landscape/DSCF1248.JPG",
+  "/images/2025/vertical/IMG_1065.JPG",
+  "/images/2025/landscape/DSCF1703.jpg",
+  "/images/2025/vertical/IMG_1115.JPG",
+  "/images/2025/landscape/DSCF2681.JPG",
+  "/images/2025/vertical/IMG_1116.JPG",
+];
