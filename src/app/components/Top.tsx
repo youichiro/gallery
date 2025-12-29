c5b7092a7cdff2b1b7ea7c993e351ba1358fb636
youichiro ogawa <cinnamon416@gmail.com>
2025-12-29 19:55:20 +0900

    スクロールボタンを追加 (#4)
    * fix
    
    * add ScrollButton
    
    * fix size
    
    * 半透明にする

--- src/app/components/Top.tsx
+++ src/app/components/Top.tsx
@@ -22,7 +22,7 @@ export default function Top() {
       />
       <div className="absolute bottom-0 right-0 z-50 p-2">
         {/* chevron-double-down icon */}
-        <a href="#gallery" className="text-slate-200 font-bold hover:text-slate-300">
+        <a href="#tabs" className="text-slate-200 font-bold hover:text-slate-300">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5" />
           </svg>
