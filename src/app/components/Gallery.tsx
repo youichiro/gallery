88f119ab6eb1f2cab8197bcac5df9d96a6df2051
youichiro <cinnamon416@gmail.com>
2025-12-29 18:25:00 +0900

    縦向きの画像に対応する

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -2,15 +2,16 @@ import Image from "next/image";
 
 export default function Gallery ({ images, onImageClick }: { images: string[], onImageClick: (image: string) => void }) {
   return (
-    <div id="gallery" className="grid grid-cols-1 gap-4 px-4 my-16 py-16 min-[440px]:grid-cols-2 lg:grid-cols-3">
+    <div id="gallery" className="columns-1 gap-4 px-4 my-16 py-16 min-[440px]:columns-2 lg:columns-3 space-y-4">
       {images.map((image, index) => (
-        <div key={index} className="relative w-full aspect-video overflow-hidden group">
+        <div key={index} className="relative w-full overflow-hidden group break-inside-avoid mb-4">
           <Image
             src={image}
             alt={image}
             priority={false}
-            fill
-            className="object-cover transition-transform duration-300 ease-in-out group-hover:scale-105 cursor-pointer"
+            width={800}
+            height={600}
+            className="w-full h-auto object-cover transition-transform duration-300 ease-in-out group-hover:scale-105 cursor-pointer"
             onClick={() => onImageClick(image)}
           />
         </div>
