8f115d885b39c96e84bd91ef0dd64061d41beaa1
youichiro <cinnamon416@gmail.com>
2025-12-29 18:32:58 +0900

    rename image directory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,35 +8,35 @@ import Gallery from "./components/Gallery";
 export default function Home() {
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
-  const images = [
-    "/images/gallery/DSC_2844.JPG",
-    "/images/gallery/DSC07886.jpeg",
-    "/images/gallery/DSC09781-2.jpg",
-    "/images/gallery/DSCF1248.JPG",
-    "/images/gallery/DSCF1703.jpg",
-    "/images/gallery/DSCF2681.JPG",
-    "/images/gallery/DSCF3442.JPG",
-    "/images/gallery/DSCF6505.JPG",
-    "/images/gallery/DSCF6513.JPG",
-    "/images/gallery/DSCF9765.jpg",
-    "/images/gallery/IMG_1015.JPG",
-    "/images/gallery/IMG_1964.JPG",
-    "/images/gallery/IMG_1967.JPG",
-    "/images/gallery/IMG_1994.JPG",
-    "/images/gallery/IMG_2025.JPG",
-    "/images/gallery/IMG_2046.JPG",
-    "/images/gallery/IMG_2363.JPG",
-    "/images/gallery/IMG_2364.JPG",
-    "/images/gallery/IMG_2472.jpg",
-    "/images/gallery/IMG_8191.JPG",
-    "/images/gallery/IMG_8787.JPG",
-    "/images/gallery/IMG_9616.JPG",
+  const images2024 = [
+    "/images/2024/DSC_2844.JPG",
+    "/images/2024/DSC07886.jpeg",
+    "/images/2024/DSC09781-2.jpg",
+    "/images/2024/DSCF1248.JPG",
+    "/images/2024/DSCF1703.jpg",
+    "/images/2024/DSCF2681.JPG",
+    "/images/2024/DSCF3442.JPG",
+    "/images/2024/DSCF6505.JPG",
+    "/images/2024/DSCF6513.JPG",
+    "/images/2024/DSCF9765.jpg",
+    "/images/2024/IMG_1015.JPG",
+    "/images/2024/IMG_1964.JPG",
+    "/images/2024/IMG_1967.JPG",
+    "/images/2024/IMG_1994.JPG",
+    "/images/2024/IMG_2025.JPG",
+    "/images/2024/IMG_2046.JPG",
+    "/images/2024/IMG_2363.JPG",
+    "/images/2024/IMG_2364.JPG",
+    "/images/2024/IMG_2472.jpg",
+    "/images/2024/IMG_8191.JPG",
+    "/images/2024/IMG_8787.JPG",
+    "/images/2024/IMG_9616.JPG",
   ]
 
   return (
     <div>
       <Top />
-      <Gallery images={images} onImageClick={(image) => setSelectedImage(image)} />
+      <Gallery images={images2024} onImageClick={(image) => setSelectedImage(image)} />
       
       {/* 選択中の画像があれば拡大表示する */}
       {selectedImage && (
