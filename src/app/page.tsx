4c93bf4c4bb5efdf1a0e577bfcf793b0dc103ae5
youichiro <cinnamon416@gmail.com>
2025-12-29 18:45:41 +0900

    定数を切り出す

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,34 +4,10 @@ import { useState } from "react";
 import Top from "./components/Top";
 import ExpandedImage from "./components/ExpandedImage";
 import Gallery from "./components/Gallery";
+import { images2024 } from "./data/images2024";
 
 export default function Home() {
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
-
-  const images2024 = [
-    "/images/2024/DSC_2844.JPG",
-    "/images/2024/DSC07886.jpeg",
-    "/images/2024/DSC09781-2.jpg",
-    "/images/2024/DSCF1248.JPG",
-    "/images/2024/DSCF1703.jpg",
-    "/images/2024/DSCF2681.JPG",
-    "/images/2024/DSCF3442.JPG",
-    "/images/2024/DSCF6505.JPG",
-    "/images/2024/DSCF6513.JPG",
-    "/images/2024/DSCF9765.jpg",
-    "/images/2024/IMG_1015.JPG",
-    "/images/2024/IMG_1964.JPG",
-    "/images/2024/IMG_1967.JPG",
-    "/images/2024/IMG_1994.JPG",
-    "/images/2024/IMG_2025.JPG",
-    "/images/2024/IMG_2046.JPG",
-    "/images/2024/IMG_2363.JPG",
-    "/images/2024/IMG_2364.JPG",
-    "/images/2024/IMG_2472.jpg",
-    "/images/2024/IMG_8191.JPG",
-    "/images/2024/IMG_8787.JPG",
-    "/images/2024/IMG_9616.JPG",
-  ]
 
   return (
     <div>
